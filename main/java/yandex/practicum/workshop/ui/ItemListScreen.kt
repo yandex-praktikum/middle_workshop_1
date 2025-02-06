32a5ee49d55b78f819f6759de960bbe1d968ae4d
Дмитрий Гордеев <netimen2@yandex.ru>
2025-02-06 14:39:36 +0300

    added state

--- main/java/yandex/practicum/workshop/ui/ItemListScreen.kt
+++ main/java/yandex/practicum/workshop/ui/ItemListScreen.kt
@@ -1,25 +1,35 @@
 package yandex.practicum.workshop.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ItemListScreen(viewModel: ItemListViewModel) {
-    val items by viewModel.uiState.collectAsState()
+    val state by viewModel.uiState.collectAsState()
 
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items(items) { item -> Item(item = item) }
+        when (val currentState = state) {
+            is State.Data -> items(currentState.items) { item -> Item(item = item) }
+            is State.Loading -> item {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
+            }
+        }
     }
 }
--- main/java/yandex/practicum/workshop/ui/ItemListViewModel.kt
+++ main/java/yandex/practicum/workshop/ui/ItemListViewModel.kt
@@ -10,18 +10,23 @@ import yandex.practicum.workshop.data.Item
 import yandex.practicum.workshop.domain.GetItemsUseCase
 import javax.inject.Inject
 
+sealed class State {
+    data object Loading : State()
+    data class Data(val page: Int, val items: List<Item>) : State()
+}
+
 @HiltViewModel
 class ItemListViewModel @Inject constructor(
     private val getItemsUseCase: GetItemsUseCase
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<List<Item>>(emptyList())
-    val uiState: StateFlow<List<Item>> = _uiState
+    private val _uiState = MutableStateFlow<State>(State.Loading)
+    val uiState: StateFlow<State> = _uiState
 
     init {
         viewModelScope.launch {
             getItemsUseCase().collect { items ->
-                _uiState.value = items
+               _uiState.value = State.Data(0, items)
             }
         }
     }
