e2c964ee6a514aeb61354a2177ca9e93a2ee200d
Дмитрий Гордеев <netimen2@yandex.ru>
2025-02-07 11:22:34 +0300

    delay depends on page size

--- main/java/yandex/practicum/workshop/data/ItemRepositoryImpl.kt
+++ main/java/yandex/practicum/workshop/data/ItemRepositoryImpl.kt
@@ -3,14 +3,8 @@ package yandex.practicum.workshop.data
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.getAndUpdate
-import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 import kotlin.math.min
 
@@ -18,7 +12,7 @@ class ItemRepositoryImpl @Inject constructor() : ItemRepository {
     private val _items = List(39) { Item(id = it, name = "Item $it") }
 
     override fun getItems(page: Int, perPage: Int): Flow<List<Item>> = flow {
-        delay(2000L)
+        delay(100L * min(perPage, _items.size))
         emit(_items.subList(page * perPage, min((page + 1) * perPage, _items.size)))
     }.flowOn(Dispatchers.IO)
 }
