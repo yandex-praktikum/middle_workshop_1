ce9375902adab9cd9a0a827c51bc43b81ce64cb9
Дмитрий Гордеев <netimen2@yandex.ru>
2025-03-25 14:38:45 +0300

    teach version

--- app/src/teach/java/yandex/practicum/workshop/ui/ItemListScreen.kt
+++ app/src/teach/java/yandex/practicum/workshop/ui/ItemListScreen.kt
@@ -1,31 +1,18 @@
 package yandex.practicum.workshop.ui
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
-
 
 @Composable
 fun ItemListScreen(viewModel: ItemListViewModel) {
-    val paginationState by viewModel.paginationState.collectAsState()
-    if (paginationState == PaginationState.LOADING) {
-        Loading()
-        return
-    }
-
     val items by viewModel.items.collectAsState()
 
     LazyColumn(
@@ -34,23 +21,5 @@ fun ItemListScreen(viewModel: ItemListViewModel) {
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(items) { item -> Item(item = item) }
-    }
-}
-
-@Composable
-private fun Loading() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-    }
-}
-
-@Composable
-private fun NoMoreItems() {
-    Box(modifier = Modifier.fillMaxWidth()) {
-        Text(
-            modifier = Modifier.align(Alignment.Center),
-            text = "Больше нету",
-            fontStyle = FontStyle.Italic
-        )
     }
 }
