a2baa0354fc111eb0364bd80345ff23dbbaba332
Дмитрий Гордеев <netimen2@yandex.ru>
2025-03-25 14:28:35 +0300

    task version

--- app/src/task/java/yandex/practicum/workshop/ui/ItemListScreen.kt
+++ app/src/task/java/yandex/practicum/workshop/ui/ItemListScreen.kt
@@ -1,18 +1,14 @@
 package yandex.practicum.workshop.ui
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.pulltorefresh.pullToRefresh
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -26,28 +22,22 @@ import androidx.compose.ui.unit.dp
 fun ItemListScreen(viewModel: ItemListViewModel) {
     val items by viewModel.items.collectAsState()
     val isRefreshing by viewModel.refreshState.collectAsState()
-
     val pullToRefreshState = rememberPullToRefreshState()
-    Column(
-        Modifier.pullToRefresh(
-            isRefreshing = isRefreshing,
-            state = pullToRefreshState,
-            onRefresh = { viewModel.loadItems() }),
-    ) {
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .height((64 * pullToRefreshState.distanceFraction).dp),
-            contentAlignment = Alignment.Center
 
-        ) {
-            if (isRefreshing)
-                Text("Загрузка...", style = MaterialTheme.typography.headlineMedium)
-            else if (pullToRefreshState.distanceFraction >= 1)
-                Text("Отпустите для обновления", style = MaterialTheme.typography.headlineMedium)
+    PullToRefreshBox(
+        isRefreshing = isRefreshing,
+        state = pullToRefreshState,
+        onRefresh = { viewModel.loadItems() },
+        indicator = {
+            Indicator(
+                modifier = Modifier.align(Alignment.TopCenter),
+                isRefreshing = isRefreshing,
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                state = pullToRefreshState
+            )
         }
-
-//    PullToRefreshBox(isRefreshing = isRefreshing, onRefresh = { viewModel.loadItems() }) {
+    ) {
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
             contentPadding = PaddingValues(16.dp),
